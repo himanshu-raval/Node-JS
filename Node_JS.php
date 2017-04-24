fd7ee47598f4582eeab6191d9a459a25c7949049
himanshu-raval <himanshuvraval@gmail.com>
2017-04-24 12:42:24 +0530

    Update Node_JS.php

--- Node_JS.php
+++ Node_JS.php
@@ -11,6 +11,27 @@ craete js file (node_test.js)
 console.log('check Node is Installed!');
 firt goto file Directory Then run Command > node node_test.js
 
+2) Ren Server in Local:
+Crate new file like main.js
+Put this code.
+//https://www.tutorialspoint.com/nodejs/nodejs_first_application.htm
+--------------------------------------
+var http = require("http"); 
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
+----------------------------------------
+
+
+
 
 
 
