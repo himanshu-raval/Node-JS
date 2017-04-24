6e6074e69e6486444b1a393f0d43082ca5a97684
himanshu-raval <himanshuvraval@gmail.com>
2017-04-24 12:20:16 +0530

    Update Node_JS.php

--- Node_JS.php
+++ Node_JS.php
@@ -1,2 +1,9 @@
 Step 1) 
 Intsall Node in Local. 
+Follow this Site :http://blog.teamtreehouse.com/install-node-js-npm-windows
+node-v6.10.2-x64.msi
+-> open CMD And Check Node is installed or not.
+-> node -v  (return installed Version of Node)
+-> npm -v  (Return Instlled Version of NPM)
+
+
