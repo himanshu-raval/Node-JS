398ab2d4547ade61c21d622f219f06609b9655ed
himanshu-raval <himanshuvraval@gmail.com>
2017-04-25 12:49:27 +0530

    Update Node_JS.php

--- Node_JS.php
+++ Node_JS.php
@@ -116,6 +116,61 @@ var server = app.listen(8081, function () {
    GET
    
  
+ --------------------------------------------------------------
+   REST API
+   
+   create users.json file with Following data
+   
+   {
+   "user1" : {
+      "name" : "mahesh",
+      "password" : "password1",
+      "profession" : "teacher",
+      "id": 1
+   },
+   "user2" : {
+      "name" : "suresh",
+      "password" : "password2",
+      "profession" : "librarian",
+      "id": 2
+   },
+   "user3" : {
+      "name" : "ramesh",
+      "password" : "password3",
+      "profession" : "clerk",
+      "id": 3
+   }
+}
+   
+   
+   Create server.js file with Following Data
+   
+   
+   var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
+   
+   
+   RUN ::> http://127.0.0.1:8081/listUsers
+   
+   now you may get json data as response.
+   
    
    
    
