670a79580c524cca7836ac97095f5a16f75f8028
himanshu-raval <himanshuvraval@gmail.com>
2017-04-25 12:21:56 +0530

    Update Node_JS.php

--- Node_JS.php
+++ Node_JS.php
@@ -54,7 +54,65 @@ npm install --save <module name>
   }
    
    
+---------------------------------
+   
+   imporant module :
+   Create Blank Folder > goto this folder in CMD then Fire following Command One by one.
+   npm init
+   npm install express --save
+   npm install body-parser --save
+   npm install cookie-parser --save
+   npm install multer --save
 
+   
+   server.js
+   
+   
+   var express = require('express');
+var app = express();
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {   
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+var server = app.listen(8081, function () {
+
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+   
+   
+   > http://127.0.0.1:8081/
+   GET
+   
  
    
    
